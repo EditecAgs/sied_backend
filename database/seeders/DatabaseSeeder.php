eceed274073b3949cc53215624aca563e6d4865d
Cristian-hericraft <christian@soluciones-hericraft.com>
2025-09-09 16:19:30 -0600

    feat :sparkles: Add sedder user

--- database/seeders/DatabaseSeeder.php
+++ database/seeders/DatabaseSeeder.php
@@ -37,6 +37,7 @@ class DatabaseSeeder extends Seeder
                 OrganizationDualProjectSeeder::class,
                 SpecialtySeeder::class,
                 StudentSeeder::class,
+                UserSeeder::class,
             ]);
         }
     }
