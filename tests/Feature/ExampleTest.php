a521f275cba60d3193edcc7310f8d6936c2e69ba
Cristian-hericraft <christian@soluciones-hericraft.com>
2025-09-09 15:04:36 -0600

    fix :bug: Duster lint

--- tests/Feature/ExampleTest.php
+++ tests/Feature/ExampleTest.php
@@ -4,14 +4,11 @@ namespace Tests\Feature;
 
 // use Illuminate\Foundation\Testing\RefreshDatabase;
 use Tests\TestCase;
+use PHPUnit\Framework\Attributes\Test;
 
 class ExampleTest extends TestCase
 {
-    /**
-     * A basic test example.
-     *
-     * @test
-     */
+    #[Test]
     public function the_application_returns_a_successful_response(): void
     {
         $response = $this->get('/');
--- tests/Unit/ExampleTest.php
+++ tests/Unit/ExampleTest.php
@@ -3,14 +3,11 @@
 namespace Tests\Unit;
 
 use PHPUnit\Framework\TestCase;
+use PHPUnit\Framework\Attributes\Test;
 
 class ExampleTest extends TestCase
 {
-    /**
-     * A basic test example.
-     *
-     * @test
-     */
+    #[Test]
     public function that_true_is_true(): void
     {
         $this->assertTrue(true);
