238b2a86ea8b652b7bca7f7d0c957fabe3ea6bf6
jessi <jessivilla657@gmail.com>
2025-12-11 16:07:24 -0600

    feat :sparkles: add route of job's cache

--- routes/api.php
+++ routes/api.php
@@ -255,6 +255,7 @@ Route::middleware(['auth:sanctum', 'token.expiration'])->group(function () {
     Route::delete('dual-projects/{id}', [DualProjectController::class, 'deleteDualProject'])->name('dual-projects-delete');
     Route::put('dual-projects/{id}', [DualProjectController::class, 'updateDualProject'])->name('dual-projects-update');
 
+    Route::get('dashboard/cache', [DashboardController::class, 'showFullCache']);
     Route::get('dual-projects/completed/count', [DashboardController::class, 'countDualProjectCompleted']);
     Route::get('students/registered/count', [DashboardController::class, 'countRegisteredStudents']);
     Route::get('projects/by-month', [DashboardController::class, 'countProjectsByMonth']);
