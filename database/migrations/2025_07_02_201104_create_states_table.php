98e484795a80092718d442c3daf8a14371128020
jessi <jessivilla657@gmail.com>
2026-02-16 13:02:40 -0600

    fix : bug : migrations whit UUID

--- database/migrations/2025_07_02_201104_create_states_table.php
+++ database/migrations/2025_07_02_201104_create_states_table.php
@@ -9,9 +9,11 @@ return new class extends Migration
     public function up(): void
     {
         Schema::create('states', function (Blueprint $table) {
+
             $table->uuid('id')->primary(); 
             $table->string('name');
             $table->timestamps();
+            
         });
     }
 
