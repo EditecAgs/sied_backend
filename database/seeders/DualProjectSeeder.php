7e18be532fb9458d7ac32cc39213d984c92b7d54
Cristian-hericraft <christian@soluciones-hericraft.com>
2026-02-25 10:03:23 -0600

    fix : bug : update seeders error

--- database/seeders/DualProjectSeeder.php
+++ database/seeders/DualProjectSeeder.php
@@ -13,44 +13,44 @@ class DualProjectSeeder extends Seeder
      */
     public function run(): void
     {
-        // $institutoAgs = Institution::where('name', 'Instituto Tecnológico de Aguascalientes')->first();
-        // $universidadAgs = Institution::where('name', 'Universidad Autónoma de Aguascalientes')->first();
-        // $ipn = Institution::where('name', 'Instituto Politécnico Nacional')->first();
-        // $institutoCdVictoria = Institution::where('name', 'Instituto Tecnológico de Ciudad Victoria')->first();
-        // $institutoSanLuis = Institution::where('name', 'Instituto Tecnológico de San Luis Potosí')->first();
+        $institutoAgs = Institution::where('name', 'Instituto Tecnológico de Aguascalientes')->first();
+        $universidadAgs = Institution::where('name', 'Universidad Autónoma de Aguascalientes')->first();
+        $ipn = Institution::where('name', 'Instituto Politécnico Nacional')->first();
+        $institutoCdVictoria = Institution::where('name', 'Instituto Tecnológico de Ciudad Victoria')->first();
+        $institutoSanLuis = Institution::where('name', 'Instituto Tecnológico de San Luis Potosí')->first();
 
-        // if (!$institutoAgs) {
-        //     $this->command->error('No se encontró el Instituto Tecnológico de Aguascalientes. Ejecuta primero InstitutionSeeder');
-        //     return;
-        // }
+        if (!$institutoAgs) {
+            $this->command->error('No se encontró el Instituto Tecnológico de Aguascalientes. Ejecuta primero InstitutionSeeder');
+            return;
+        }
 
-        // $dualProjects = [
-        //     ['institution' => $institutoAgs, 'number_student' => 2],
-        //     ['institution' => $institutoAgs, 'number_student' => 0],
-        //     ['institution' => $institutoAgs, 'number_student' => 0],
-        //     ['institution' => $institutoAgs, 'number_student' => 0],
-        //     ['institution' => $institutoAgs, 'number_student' => 0],
-        //     ['institution' => $universidadAgs, 'number_student' => 0],
-        //     ['institution' => $universidadAgs, 'number_student' => 0],
-        //     ['institution' => $ipn, 'number_student' => 0],
-        //     ['institution' => $institutoCdVictoria, 'number_student' => 0],
-        //     ['institution' => $institutoSanLuis, 'number_student' => 0],
-        // ];
+        $dualProjects = [
+            ['institution' => $institutoAgs, 'number_student' => 2],
+            ['institution' => $institutoAgs, 'number_student' => 0],
+            ['institution' => $institutoAgs, 'number_student' => 0],
+            ['institution' => $institutoAgs, 'number_student' => 0],
+            ['institution' => $institutoAgs, 'number_student' => 0],
+            ['institution' => $universidadAgs, 'number_student' => 0],
+            ['institution' => $universidadAgs, 'number_student' => 0],
+            ['institution' => $ipn, 'number_student' => 0],
+            ['institution' => $institutoCdVictoria, 'number_student' => 0],
+            ['institution' => $institutoSanLuis, 'number_student' => 0],
+        ];
 
-        // foreach ($dualProjects as $project) {
-        //     if (!$project['institution']) continue;
-        //     DualProject::updateOrCreate(
-        //         [
-        //             'id_institution' => $project['institution']->id,
-        //             'number_student' => $project['number_student']
-        //         ],
-        //         [
-        //             'has_report' => 1,
-        //         ]
-        //     );
-        // }
+        foreach ($dualProjects as $project) {
+            if (!$project['institution']) continue;
+            DualProject::updateOrCreate(
+                [
+                    'id_institution' => $project['institution']->id,
+                    'number_student' => $project['number_student']
+                ],
+                [
+                    'has_report' => 1,
+                ]
+            );
+        }
 
-        // $this->command->info('Proyectos duales creados/actualizados correctamente');
+        $this->command->info('Proyectos duales creados/actualizados correctamente');
 
         $this->command->info('DualProjectSeeder está desactivado - No se crearon proyectos');
     }
