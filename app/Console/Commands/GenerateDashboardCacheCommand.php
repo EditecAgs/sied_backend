ca775bf0ed6a0bda0629bc0d2731c5fc9fea0607
Cristian-hericraft <christian@soluciones-hericraft.com>
2026-02-25 10:27:30 -0600

    feat :sparkles: command to generate job

---
+++ app/Console/Commands/GenerateDashboardCacheCommand.php
@@ -0,0 +1,21 @@
+<?php
+
+namespace App\Console\Commands;
+
+use Illuminate\Console\Command;
+use App\Jobs\GenerateDashboardCacheJob;
+
+class GenerateDashboardCacheCommand extends Command
+{
+    protected $signature = 'dashboard:generate-cache';
+    protected $description = 'Genera manualmente el cache del dashboard';
+
+    public function handle()
+    {
+        GenerateDashboardCacheJob::dispatch();
+
+        $this->info('Job GenerateDashboardCacheJob enviado a la cola.');
+
+        return Command::SUCCESS;
+    }
+}
